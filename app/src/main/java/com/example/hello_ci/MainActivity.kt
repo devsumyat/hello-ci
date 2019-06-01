4c809881040a24cc965846a3bcc17cd4eaff7037
su myat <sumyat@sus-MacBook-Pro.local>
2019-06-01 13:40:15 +0630

    testlab with ci

--- app/src/main/java/com/example/hello_ci/MainActivity.kt
+++ app/src/main/java/com/example/hello_ci/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.hello_ci
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import android.util.StatsLog.logEvent
+import android.R.id
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        analyticsEvents()
+    }
+
+    private fun analyticsEvents(){
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id010")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "su su")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        mFirebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
